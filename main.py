6ba3fca4d05c0fd53edbde3eb65a35c6181fb855
dadereindraf <dadereindraf@gmail.com.com>
2024-08-01 22:00:32 +0700

    ganti nama

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from io import BytesIO
 def main():
     st.title("KODE POS")
 
-    st.write("Pilih file Excel untuk Data1:")
+    st.write("Pilih file Excel untuk data OSS:")
     uploaded_file_data1 = st.file_uploader("Upload File", type=["xlsx", "xls"])
 
-    st.write("Pilih file Excel untuk ListKodePOS_filtered:")
+    st.write("Pilih file Excel untuk ListKodePOS:")
     uploaded_file_listkodepos = st.file_uploader("Upload File", type=["xlsx", "xls"], key="listkodepos")
 
     if uploaded_file_data1 is not None and uploaded_file_listkodepos is not None:
